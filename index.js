f0d8fd6502a11550de61f3d05b98370da18b564b
berryboylb <phemmynesce4life@gmail.com>
2022-06-21 23:27:00 +0100

    feat: Display person details

---
+++ index.js
@@ -0,0 +1,9 @@
+const person = {
+  name: "Daramola Olorunfemi",
+  height: 187.96,
+  country: "Nigeria",
+};
+
+console.log(
+  `Hello my name is ${person.name}, I'm from ${person.country} and my height is about ${person.height}cm. Nice to meet you.`
+);
